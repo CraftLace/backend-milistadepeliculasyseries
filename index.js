f16817bca891408807b28a8d31789f848c7c5068
Christopher Parrales <lecristianot@gmail.com>
2022-07-06 22:34:39 -0500

    uso de env con mongodb

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const express = require("express");
 const app = express();
 //create port
 const port = process.env.PORT || 3000;
+require("dotenv").config();
 //create route
 app.get("/", (req, res) => res.send("Hola Mundo!"));
+//conectarse a mongo
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.log(err));
 //start server
-let url = process.env.api_key_tmdb;
 app.listen(port, () =>
   console.log(`App de ejemplo en el puerto http://localhost:${port}!`)
 );
